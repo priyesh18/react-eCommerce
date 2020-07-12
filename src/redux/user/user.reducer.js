7840e9c1f4a5fc7ddc6c9e6d5724a272f0467495
Priyesh <safal.priyesh@gmail.com>
2020-07-12 12:50:00 -0400

    redirect user after signin

--- src/redux/user/user.reducer.js
+++ src/redux/user/user.reducer.js
@@ -1,10 +1,11 @@
+import { UserActionTypes } from './user.types'
 const INITIAL_STATE = {
     currentUser: null
 }
 // If state is undefined (not null) then take INITIAL_STATE as the default value
 const userReducer = (state = INITIAL_STATE, action) => {
     switch(action.type) {
-        case 'SET_CURRENT_USER':
+        case UserActionTypes.SET_CURRENT_USER:
             return {
                 ...state,
                 currentUser: action.payload
