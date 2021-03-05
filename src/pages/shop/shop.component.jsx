1ed0f7212d34715c8ff50d4f23006d2920de91c5
Priyesh Patel <safal.priyesh@gmail.com>
2021-03-05 11:26:41 -0500

    add routing and collection page

--- src/pages/shop/shop.component.jsx
+++ src/pages/shop/shop.component.jsx
@@ -1,12 +1,16 @@
 import React from "react";
 import { connect } from "react-redux";
+import { Route } from "react-router-dom";
 import { createStructuredSelector } from "reselect";
 import { selectCollections } from "../../redux/shop/shop.selectors";
 import CollectionsOverview from "../../components/collections-overview/collections-overview.component";
+import CollectionPage from "../collection/collection.component";
 
-const ShopPage = ({ collections }) => (
+const ShopPage = ({ match }) => (
+  // route automatically passes match, location and history
   <div className="shop-page">
-    <CollectionsOverview />
+    <Route exact path={`${match.path}`} component={CollectionsOverview} />
+    <Route path={`${match.path}/:collectionId`} component={CollectionPage} />
   </div>
 );
 
--- src/redux/shop/shop.selectors.js
+++ src/redux/shop/shop.selectors.js
@@ -5,4 +5,20 @@ const selectShop = state => state.shop;
 export const selectCollections = createSelector(
   [selectShop],
   shop => shop.collections
-)
+);
+
+export const selectCollectionsForPreview = createSelector(
+  [selectCollections],
+  collections => Object.keys(collections).map(key => collections[key])
+);
+
+export const selectCollection = collectionUrlParam => {
+  console.log(collectionUrlParam)
+  return (
+    createSelector(
+      [selectCollections],
+      collections => collections[collectionUrlParam]
+    )
+
+  )
+}
